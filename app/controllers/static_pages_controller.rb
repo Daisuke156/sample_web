07e7575961d2a660925b283778ec3bf78dde51d2
Daisuke <dixiy777@yahoo.co.jp>
2020-10-05 15:31:45 +0000

    createHomepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,15 @@
 class StaticPagesController < ApplicationController
+  
+  def home
+  end
+  
+  def about
+  end
+  
+  def menu
+  end
+  
+  def event
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#home' # => root_path
+  get  '/about',  to: 'static_pages#about'
+  get  '/menu',  to: 'static_pages#menu'
+  get  '/event',  to: 'static_pages#event'
+  
 end
