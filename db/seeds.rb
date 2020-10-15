c8b3e68c00f63047e7e30e085fb2e1eaa8625753
Daisuke <dixiy777@yahoo.co.jp>
2020-10-15 17:37:37 +0000

    Add menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,28 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Games::Pokemon.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = Faker::Internet.email
   password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password)
 end
+
+categorys = ["main","drink","dessert"]
+10.times do |m|
+  name = Faker::Food.dish
+  price = Faker::Number.within(range: 500..3000)
+  calorie = Faker::Number.within(range: 100..1100)
+  category = categorys.sample
+  coment = Faker::Food.description
+  Menu.create!(name: name,
+              price: price,
+              calorie: calorie,
+              category: category,
+              coment: coment)
+end
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
